d494c70ffc0b6c32a575b8ecb1d9e66566ae9686
PJ Medina <paulo@healthnow.ph>
2023-06-09 21:49:11 +0800

    fixed untracked files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,95 @@
-'use client';
+import Image from 'next/image'
+import styles from './page.module.css'
 
-/*
- * ---------------------------------------------
- * Author: PJ Medina
- * Date:   Thursday June 8th 2023
- * Last Modified by: PJ Medina - <paulo@healthnow.ph>
- * Last Modified time: June 9th 2023, 8:58:16 pm
- * ---------------------------------------------
- */
-
-import { Button } from '@mui/material';
-import { db } from '../config/firebase';
-import { collection, getDocs, query, onSnapshot, doc } from 'firebase/firestore';
-
-export default async function Home() {
-  const callFirebase = async () => {
-    console.log('calling firebase');
-
-    const ref = collection(db, 'categories');
-    const q = query(ref);
-    const querySnapshot = await getDocs(q);
-
-    if (querySnapshot.docs.length === 0) {
-      console.log('EMPTY');
-    }
-
-    const account = querySnapshot.docs.map(doc => ({
-      ...doc.data(),
-      id: doc.id,
-    }));
-
-    console.log({ categories: account });
-  };
-
+export default function Home() {
   return (
-    <div>
-      <Button onClick={callFirebase}>Category Pages</Button>
-    </div>
-  );
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore the Next.js 13 playground.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
 }
